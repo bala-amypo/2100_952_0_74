6eb5b0a09ad19b80226d76190877cbaf77777b08
bala-amypo <bala@amypo.in>
2025-12-17 08:04:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -10,7 +10,7 @@ import com.example.demo.service.StudentService;
 public class StudentServiceImpl implements StudentService{
   @Override
    public Student poststudent(Student st){
-
+       return 
    } 
 }
-    
+     
